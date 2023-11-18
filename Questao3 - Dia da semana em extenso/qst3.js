9f1857e46d60d6fc6c985e82eb85a4f9f50e18ea
DanielHermes <145810350+DanielHermesGT@users.noreply.github.com>
2023-11-18 01:56:38 -0300

    Delete Questao3 - Dia da semana em extenso directory

--- Questao3 - Dia da semana em extenso/qst3.js
+++
@@ -1,7 +0,0 @@
-function data(){
-    let datauser = document.getElementById("datauser").value
-    const dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"];
-    const data = new Date(datauser)
-    
-    document.getElementById("mostrar").innerHTML = dias[data.getDay() + 1]
-}   
